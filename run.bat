17c9f013bd52897d0705d3aa864ed3b8e80fdce3
Danil Kolumbet <kolumbetko@gmail.com>
2021-02-27 00:28:15 +0200

    Copypaste code from opengl-pp and learnopengl.com and msdn

--- run.bat
+++ run.bat
@@ -1,7 +1,10 @@
 @echo off
+rem /O2
+set CFLAGS=/std:c11 /FC /W4 /WX /Zl /D_USE_MATH_DEFINES /wd4996 /nologo
+set INCLUDES=/I Dependencies\GLFW\include /I Dependencies\GLEW\include
+set LIBS=Dependencies\GLFW\lib\glfw3.lib Dependencies\GLEW\lib\glew32s.lib opengl32.lib User32.lib Gdi32.lib Shell32.lib
 
-cl src\main.c /Fo:build\ /Fe:build\zoomer.exe /nologo ^
-    /link Shell32.lib Gdi32.lib User32.lib /OUT:build\zoomer.exe -SUBSYSTEM:console
+cl %CFLAGS% %INCLUDES% src\main.c /Fo:build\ /Fe:build\zoomer.exe %LIBS% /link /NODEFAULTLIB:libcmt.lib
 
 if %errorlevel% == 0 (
     .\build\zoomer 2>&1 1 > build\output.log
--- src/main.c
+++ src/main.c
@@ -1,14 +1,109 @@
 #include "windows.h"
 #include <stdio.h>
+#include <math.h>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#define GL_GLEXT_PROTOTYPES
+#include <GLFW/glfw3.h>
+
+const char *shader_type_as_cstr(GLenum shader_type)
+{
+    switch (shader_type) {
+    case GL_VERTEX_SHADER:   return "GL_VERTEX_SHADER";
+    case GL_GEOMETRY_SHADER: return "GL_GEOMETRY_SHADER";
+    case GL_FRAGMENT_SHADER: return "GL_FRAGMENT_SHADER";
+    default:                 return "<unknown>";
+    }
+}
+
+GLuint compile_shader(const char *source_code, GLenum shader_type)
+{
+    GLuint shader = 0;
+    shader = glCreateShader(shader_type);
+    if (shader == 0) {
+        fprintf(stderr, "Could not create a shader %s",
+                shader_type_as_cstr(shader_type));
+        exit(1);
+    }
+
+    glShaderSource(shader, 1, &source_code, 0);
+    glCompileShader(shader);
+
+    GLint compiled = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    if (!compiled) {
+        GLchar buffer[1024];
+        int length = 0;
+        glGetShaderInfoLog(shader, sizeof(buffer), &length, buffer);
+        fprintf(stderr, "Could not compile shader %s: %s\n",
+                shader_type_as_cstr(shader_type), buffer);
+        exit(1);
+    }
+
+    return shader;
+}
+
+GLuint link_program(GLuint vertex_shader, GLuint fragment_shader)
+{
+    GLuint program = glCreateProgram();
+
+    if (program == 0) {
+        fprintf(stderr, "Could not create shader program");
+        exit(1);
+    }
+
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    GLint linked = 0;
+    glGetProgramiv(program, GL_LINK_STATUS, &linked);
+    if (!linked) {
+        GLchar buffer[1024];
+        int length = 0;
+        glGetProgramInfoLog(program, sizeof(buffer), &length, buffer);
+        fprintf(stderr, "Could not link the program: %s\n", buffer);
+        exit(1);
+    }
+
+    return program;
+}
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec2 aTexCoord;\n"
+    "\n"
+    "out vec3 ourColor;\n"
+    "out vec2 TexCoord;\n"
+    "\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "\n"
+    "in vec3 ourColor;\n"
+    "in vec2 TexCoord;\n"
+    "\n"
+    "uniform sampler2D screenshotTexture;"
+    "\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = texture(screenshotTexture, TexCoord);\n"
+    "}\n\0";
 
 int main(int argc, char const *argv[])
 {
     (void) argc;
     (void) argv;
-    
+
     SetProcessDPIAware();
-    printf("%s\n", "Start");
-    printf("%s\n", "Start1");
+
+    // https://stackoverflow.com/a/28248531
     int x1, y1, x2, y2, w, h;
 
     // get screen dimensions
@@ -24,15 +119,130 @@ int main(int argc, char const *argv[])
     HDC     hDC     = CreateCompatibleDC(hScreen);
     HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, w, h);
     HGDIOBJ old_obj = SelectObject(hDC, hBitmap);
-    BOOL    bRet    = BitBlt(hDC, 0, 0, w, h, hScreen, x1, y1, SRCCOPY);
+    BitBlt(hDC, 0, 0, w, h, hScreen, x1, y1, SRCCOPY);
 
-    // save bitmap to clipboard
-    OpenClipboard(NULL);
-    EmptyClipboard();
-    SetClipboardData(CF_BITMAP, hBitmap);
-    CloseClipboard();   
+    if (!glfwInit()) {
+        fprintf(stderr, "Could not initialize GLFW!\n");
+        exit(1);
+    }
 
-    // clean up
+    GLFWwindow *window = glfwCreateWindow(
+        w, h,
+        "Zoomer", NULL, NULL);
+    if (window == NULL) {
+        glfwTerminate();
+        fprintf(stderr, "Could not create GLFW window!\n");
+        exit(1);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (GLEW_OK != glewInit()) {
+        fprintf(stderr, "Could not initialize GLEW!\n");
+        exit(1);
+    }
+
+    GLuint vertexShader = compile_shader(vertexShaderSource, GL_VERTEX_SHADER);
+    GLuint fragmentShader = compile_shader(fragmentShaderSource, GL_FRAGMENT_SHADER);
+    GLuint shaderProgram = link_program(vertexShader, fragmentShader);
+
+    float vertices[] = {
+        // positions          // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    unsigned int vertexBufferObject, vertexArrayObject, elementBufferObject;
+    glGenVertexArrays(1, &vertexArrayObject);
+    glGenBuffers(1, &vertexBufferObject);
+    glGenBuffers(1, &elementBufferObject);
+
+    glBindVertexArray(vertexArrayObject);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    GLuint screenshotTexture;
+
+    glGenTextures(1, &screenshotTexture);
+    glBindTexture(GL_TEXTURE_2D, screenshotTexture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    BITMAP bitmap;
+    GetObject(hBitmap, sizeof(bitmap), (void *) &bitmap);
+    BITMAPINFO MyBMInfo = {0};
+    MyBMInfo.bmiHeader.biSize = sizeof(MyBMInfo.bmiHeader);
+    if(0 == GetDIBits(hDC, hBitmap, 0, 0, NULL, &MyBMInfo, DIB_RGB_COLORS))
+    {
+        printf("%s\n", "couldn't get DIBits to MyBMInfo");
+    }
+    BYTE* lpPixels = (BYTE*) malloc(MyBMInfo.bmiHeader.biSizeImage);
+    if (lpPixels == 0) {
+        printf("ERROR: Out of memory\n");
+        return 1;
+    }
+
+    MyBMInfo.bmiHeader.biBitCount = 32;
+    MyBMInfo.bmiHeader.biCompression = BI_RGB;
+    MyBMInfo.bmiHeader.biHeight = abs(MyBMInfo.bmiHeader.biHeight);
+    if(0 == GetDIBits(hDC, hBitmap, 0, MyBMInfo.bmiHeader.biHeight,
+              lpPixels, &MyBMInfo, DIB_RGB_COLORS)) {
+        printf("%s\n", "couldn't get DIBits to lpPixels");
+    }
+    if (lpPixels) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, lpPixels);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        free(lpPixels);
+    } else {
+        printf("%s\n", "failed to load texture from lpPixels");
+
+    }
+
+    glUseProgram(shaderProgram);
+    glUniform1i(glGetUniformLocation(shaderProgram, "screenshotTexture"), 0);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vertexArrayObject);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     SelectObject(hDC, old_obj);
     DeleteDC(hDC);
     ReleaseDC(NULL, hScreen);
