dfb111e20308dd0049def3e9a110467ff9b4c130
Danil Kolumbet <kolumbetko@gmail.com>
2021-02-27 22:10:50 +0200

    Clean up build script

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,14 +1,12 @@
 @echo off
-rem /O2
-set CFLAGS=/std:c11 /FC /W4 /WX /Zl /D_USE_MATH_DEFINES /wd4996 /nologo
+set CFLAGS=/std:c11 /O2 /FC /W4 /WX /Zl /D_USE_MATH_DEFINES /wd4996 /nologo
 set INCLUDES=/I Dependencies\GLFW\include /I Dependencies\GLEW\include
 set LIBS=Dependencies\GLFW\lib\glfw3.lib Dependencies\GLEW\lib\glew32s.lib opengl32.lib User32.lib Gdi32.lib Shell32.lib
 
 cl %CFLAGS% %INCLUDES% src\zoomer.c /Fo:build\ /Fe:build\zoomer.exe %LIBS% /link /NODEFAULTLIB:libcmt.lib
 
-if %errorlevel% == 0 (
-    .\build\zoomer 2>&1 1 > build\output.log
-    SET /p output_variable=<build\output.log
+if "%1" == "-run" (
+    .\build\zoomer.exe 2>&1 1 > .\build\output.log
     @echo on
-    type build\output.log
+    type .\build\output.log
 )
